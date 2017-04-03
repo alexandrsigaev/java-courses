705e2db6e8f3a68191f8fd44bc4d24601d6b8c9b
Aleksandr Sigaev <sigaev.aleksandr.v@yandex.ru>
2017-04-04 02:13:20 +0300

    Refactor Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,93 @@
-public class Calculate{
-	public static void main(String[] arg){
-		System.out.println("Calculate");
-	}
+public class Calculate {
+    public static void main(String[] args) {
+
+        double first = Double.valueOf(args[0]);
+        double second = Double.valueOf(args[1]);
+
+        sum(first, second);
+        subtract(first, second);
+        multi(first, second);
+        division(first, second);
+
+    }
+
+    public static void sum (int a, int b){
+        System.out.print(a + " + " + b + " = ");
+        System.out.println(a + b);
+    }
+
+    public static void sum (char a, char b){
+        System.out.print(a + " + " + b + " = ");
+        System.out.println(a + b);
+    }
+
+    public static void sum (double a, double b){
+        System.out.print(a + " + " + b + " = ");
+        System.out.println(a + b);
+    }
+
+    public static void sum (long a, long b){
+        System.out.print(a + " + " + b + " = ");
+        System.out.println(a + b);
+    }
+
+    public static void subtract (int a, int b){
+        System.out.print(a + " - " + b + " = ");
+        System.out.println(a - b);
+    }
+
+    public static void subtract (char a, char b){
+        System.out.print(a + " - " + b + " = ");
+        System.out.println(a - b);
+    }
+
+    public static void subtract (double a, double b){
+        System.out.print(a + " - " + b + " = ");
+        System.out.println(a - b);
+    }
+
+    public static void subtract (long a, long b){
+        System.out.print(a + " - " + b + " = ");
+        System.out.println(a - b);
+    }
+
+    public static void multi (int a, int b){
+        System.out.print(a + " * " + b + " = ");
+        System.out.println(a * b);
+    }
+
+    public static void multi (char a, char b){
+        System.out.print(a + " * " + b + " = ");
+        System.out.println(a * b);
+    }
+
+    public static void multi (double a, double b){
+        System.out.print(a + " * " + b + " = ");
+        System.out.println(a * b);
+    }
+
+    public static void multi (long a, long b){
+        System.out.print(a + " * " + b + " = ");
+        System.out.println(a * b);
+    }
+
+    public static void division (int a, int b){
+        System.out.print(a + " / " + b + " = ");
+        System.out.println(a / b);
+    }
+
+    public static void division (char a, char b){
+        System.out.print(a + " / " + b + " = ");
+        System.out.println(a / b);
+    }
+
+    public static void division (double a, double b){
+        System.out.print(a + " / " + b + " = ");
+        System.out.println(a / b);
+    }
+
+    public static void division (long a, long b){
+        System.out.print(a + " / " + b + " = ");
+        System.out.println(a / b);
+    }
 }
